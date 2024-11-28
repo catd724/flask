2e26490a5b8f1489a799943c9666f948457278f0
Kashif <catd724@gmail.com>
2024-11-28 20:17:09 +0500

    update main.py to handle form submissions and rename home.html to name.html

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,request
 from markupsafe import escape
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def mainpage():
-    # return f'''<h1> Home </h1>
-    # <a href = "{url_for('home',name = 'kashif')}">kashif</a> '''
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 
 
-@app.route("/<name>")
-def home(name):
+@app.route("/name", methods=["POST","GET"])
+def namepage():
 
-    return f"<p>Hello, World! {escape(name)}</p>"
+    pname = request.form.get("pname","[No name entered!]")
+    page = request.form.get("page","[No age entered!]")
+    pemail = request.form.get("pemail","[No email entered!!]")
+
+    return render_template("name.html",pname=pname,page=page,pemail=pemail)
 
 
-@app.route("/post/<int:post_id>")
-def post(post_id):
-    return f"you are reading post with post_id {escape(post_id)}"
+# @app.route("/post/<int:post_id>")
+# def post(post_id):
+#     return f"you are reading post with post_id {escape(post_id)}"
 
-@app.route("/about/")
-def about():
-    return "<h1>about</h1>"
+# @app.route("/about/")
+# def about():
+#     return "<h1>about</h1>"
 
-with app.test_request_context():
-    print(url_for("home",name =  "kashif"))
-    print(url_for("post",post_id = '1234'))
+# with app.test_request_context():
+#     print(url_for("namepage",pname =  "kashif"))
+#     print(url_for("post",post_id = '1234'))
