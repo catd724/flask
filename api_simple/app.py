2e6879ee39cc8c4e3a1e56ebb513321c2fa22695
Kashif <catd724@gmail.com>
2024-12-06 18:28:58 +0500

    deleted a file

---
+++ api_simple/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify,request,render_template,redirect
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/google/flan-t5-large"
+headers = {"Authorization": "Bearer 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"}
+payload = {
+     "inputs":None
+}
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+
+app = Flask(__name__)
+
+test = {"name":"Kashif", "degree":"BSCS"}
+@app.route("/")
+def chatbot():
+
+    return render_template("chatbot.html",response=None)
+
+@app.route("/question" , methods=["POST","GET"])
+def new_user():
+    if request.method == "POST":
+        question = request.form.get("question")
+        payload["inputs"] = question
+        response = query(payload=payload)
+        answer = response[0]["generated_text"]
+        
+
+    return render_template("chatbot.html", response=answer)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
